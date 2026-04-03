98468cbba16658780779d17bd29b90fc4c2be51e
uneven74 <ahtnezev@gmail.com>
2026-04-02 22:17:03 -0600

    [U] config of reducer fn

--- src/05-useReducer/reducer/tasksReducer.ts
+++ src/05-useReducer/reducer/tasksReducer.ts
@@ -24,8 +24,60 @@ export const tasksReducer = (
     state: TaskState,
     action: TaskAction
 ): TaskState => {
+    //* regulary we use switch case in reducers
+    switch (action.type) {
+        case 'ADD_TODO': {
+            const newTodo: Todo = {
+                id: Date.now(),
+                text: action.payload,
+                completed: false
+            };
 
-    
+            //! dont use push
+            // state.todos.push(newTodo); // mutating the state -> not recommended
+            return {
+                ...state,
+                todos: [...state.todos, newTodo],
+                length: state.todos.length + 1,
+                pending: state.pending + 1,
+            };
+        }
+        case 'DELETE_TODO': {
+            const currentTodos = state.todos.filter((todo) => todo.id !== action.payload);
+            const completedTodos = currentTodos.filter(todo => todo.completed).length;
+            const pendingTodos = currentTodos.length - completedTodos;
+
+            return {
+                ...state,
+                todos: currentTodos,
+                length: currentTodos.length,
+                completed: completedTodos,
+                pending: pendingTodos,
+            };
+        }
+        case 'TOGGLE_TODO': {
+            const updatedTodos = state.todos.map((todo) => {
+            if (todo.id === action.payload) {
+                return {
+                ...todo,
+                completed: !todo.completed
+                }
+            }
+                return todo;
+            });
+            
+            return {
+                ...state,
+                todos: updatedTodos,
+                completed: updatedTodos.filter(todo => todo.completed).length,
+                pending: updatedTodos.filter(todo => !todo.completed).length,
+            }
+        }
+        default:
+            return state;
+    }
+
+
 
     return state;
 };
