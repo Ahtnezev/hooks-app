5681bc21b5e9a3aefb2d33f1fbd4729130198289
uneven74 <ahtnezev@gmail.com>
2026-04-15 23:33:25 -0600

    [U] user context 1.1

--- src/09-useContext/context/UserContext.tsx
+++ src/09-useContext/context/UserContext.tsx
@@ -1,11 +1,44 @@
-import {type FC, type PropsWithChildren } from "react"
+import {createContext, useState, type FC, type PropsWithChildren } from "react"
+import type { User } from "../data/user-mock.data";
 
 // interface UserContextProps {
 //     children: React.ReactNode;
 // }
 
+type AuthStatus = 'checking' | 'authenticated' | 'not-authenticated';
+
+interface UserContextProps {
+    // state
+    authStatus: AuthStatus;
+    user: User | null;
+
+    // methods
+    login: (userId: number)  => boolean;
+    logout: () => void;   
+}
+
+export const UserContext = createContext({} as UserContextProps);
+
+
 // nos permite compartir algun tipo de comportamiento o estado
 // de preferencia no colocar html, el provider es mas para logica de negocio
+// proveedor es un: HoC -> High-Order Component
 export const UserContextProvider: FC<PropsWithChildren> = ({children} ) => { //:UserContextProps || : PropsWithChildren
-  return <>{ children }</>;
+    const [authStatus, setAuthStatus] = useState<AuthStatus>('checking');
+    const [user, setUser] = useState<User|null>(null);
+
+    const handleLogin = (userId: number) => {
+        console.log({userId});
+        return true;
+    }
+    const handleLogout = () => {
+        console.log('LOGOUT');
+    }
+
+  return <UserContext value={{
+    authStatus: 'checking',
+    user: user,
+    login: handleLogin,
+    logout: handleLogout
+  }}>{ children }</UserContext>;
 }
